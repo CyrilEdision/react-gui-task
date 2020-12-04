f1145809ace89751f6043e70dd24fa8b68b2d217
CyrilEdision <vcyriledison@gmail.com>
2020-12-05 00:35:28 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
-
+import { Draggable, Droppable } from "react-drag-and-drop";
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      componentPosition: "cyril"
+      componentPosition: "cyril",
+      dropped: ""
     };
-    
+
     localStorage.setItem("rememberMe", this.state.componentPosition);
   }
   render() {
@@ -22,37 +23,49 @@ export default class App extends Component {
             <div className="col-12 col-sm-3 uicomponent-head">
               <div className="uicomponent">
                 <ul>
-                  <li>
-                    <div className="icon">A</div>
-                    <div className="contentHeading">Heading</div>
-                  </li>
-                  <li>
-                    <div className="icon">P</div>
-                    <div className="contentHeading">Paragraph</div>
-                  </li>
-                  <li>
-                    <div className="icon">B</div>
-                    <div className="contentHeading">Button</div>
-                  </li>
+                  <Draggable type="form" data="Heading">
+                    <li>
+                      <div className="icon">A</div>
+                      <div className="contentHeading">Heading</div>
+                    </li>
+                  </Draggable>
+                  <Draggable type="form" data="Paragraph">
+                    <li>
+                      <div className="icon">P</div>
+                      <div className="contentHeading">Paragraph</div>
+                    </li>
+                  </Draggable>
+                  <Draggable type="form" data="Button">
+                    <li>
+                      <div className="icon">B</div>
+                      <div className="contentHeading">Button</div>
+                    </li>
+                  </Draggable>
                 </ul>
+                <ul />
               </div>
             </div>
+
             <div className="col-12 col-sm-9 fields-head">
-              <div className="fields" />
+              <Droppable types={["form"]} onDrop={this.onDrop.bind(this)}>
+                <div className="fields">
+                  <ul className="Smoothie">
+                    <li>
+                      <div style={{ textAlign: "center" }}>
+                        {this.state.dropped}
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </Droppable>
             </div>
           </div>
         </div>
       </>
     );
   }
-  save(form) {
-    // you will receive form
-    console.log(form);
+  onDrop(data) {
+    console.log(data);
+    this.setState({ dropped: data.form });
   }
-
-  // updateForm(callback){
-  //     // fetch form and set it to callback
-  //     let form = axios.......
-  //     callback(form)
-  // }
 }
